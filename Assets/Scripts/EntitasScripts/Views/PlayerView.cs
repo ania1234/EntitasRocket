cc49f8d364a81baeda11840c20c9542a161b3393
ania1234 <anna.sierhej@gmail.com>
2023-07-03 21:53:40 +0200

    Added guideline to entitas rocket

--- Assets/Scripts/EntitasScripts/Views/PlayerView.cs
+++ Assets/Scripts/EntitasScripts/Views/PlayerView.cs
@@ -1,8 +1,15 @@
+using Entitas;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerView : View
 {
+    [SerializeField]
+    private SineRenderingScript _sine;
 
+    private void Update()
+    {
+        _sine.UpdatePoints(_linkedEntity.sineMovement.A, _linkedEntity.sineMovement.B, _linkedEntity.sineMovement.C);
+    }
 }
